39a0f3b0a1ae66672fa3d9ecd60d52ab0d1180e3
akanksha6082 <56785692+akanksha6082@users.noreply.github.com>
2020-05-23 16:05:57 +0530

    Clock implementation using threads in C language
    Concurrent programming is achieved through multi-threading. Threads are the basic unit of execution in the processes. Threads generally have an id, program counter, Register set and Stack.
    The above code implements a simple digital clock using pthread library of C which provides multi threading features.
    Mutex and Semaphores are generally used for synchronization of the threads to solve the problem of the critical section in multi threading
    A critical section is a section of code that is executed by multiple threads and where the sequence of execution for the threads makes a difference in the result of the concurrent execution of the critical section.

--- assignment_09/thread_clock.c
+++ assignment_09/thread_clock.c
@@ -1,1 +1,105 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<pthread.h>
+#include<time.h>
+
+
+
+
+int hour = 0, minutes = 0 , seconds = 0;
+
+void * calc_hour(){
+	
+	while(1){
+		
+		if(minutes == 60){
+			
+			minutes  = 0;
+			seconds  = 0;
+		    if(hour == 24){
+				hour = 0; 			
+			}
+			else{
+				hour++;
+			}
+					
+		}	
+		
+	}
+		
+}
+
+void * calc_min(){
+	
+	while(1){
+
+		if(seconds == 60){
+			
+			minutes ++;
+			seconds = 0;	
+		}
+		
+	}
+	
+}
+
+void * calc_sec(){
+
+
+	while(1){
+		
+		if(seconds != 60){
+			sleep(1);
+			seconds++;	
+		}
+		
+	}
+}
+
+
+
+void * display(){
+
+	pthread_t thour, tmin, tsec;
+	
+	pthread_create(&tsec, NULL, calc_sec, NULL);
+	pthread_create(&tmin, NULL, calc_min, NULL);
+	pthread_create(&thour, NULL, calc_hour, NULL);
+	
+	while(1){
+		
+		printf("\r %02d : %02d : %02d", hour, minutes, seconds);	
+	}
+
+	// wait for the threads to execute
+	pthread_join(tsec, NULL);
+	pthread_join(tmin, NULL);
+	pthread_join(thour, NULL);
+	
+	return NULL;		
+}
+
+
+int main(int argc, char * argv[]){
+	
+	time_t now;
+	time(&now);
+
+	
+
+	struct tm *local_time = localtime(&now);
+
+	// systems local time
+	hour = local_time -> tm_hour;
+	minutes = local_time -> tm_min;
+	seconds = local_time -> tm_sec;
+	
+	pthread_t tmain;
+	
+	pthread_create(&tmain, NULL, display, NULL );
+	pthread_join(tmain, NULL);
+
+	return 0;
+}
 
